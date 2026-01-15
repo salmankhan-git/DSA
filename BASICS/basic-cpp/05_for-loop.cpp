4952d1d73589171c074f8423722d3a822e679f76
salmankhan-git <salmankhan.techky@gmail.com>
2026-01-15 18:05:41 +0530

    loops

---
+++ BASICS/basic-cpp/05_for-loop.cpp
@@ -0,0 +1,13 @@
+/*
+for loop is a control structure in programming that allows you to execute a specific block of code repeatedly. It's especially useful when you want to perform the same task multiple times without duplicating your code
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        cout << "Hey, Striver, this is the " << i << "'th iteration" << endl;
+    }
+    return 0;
+}
