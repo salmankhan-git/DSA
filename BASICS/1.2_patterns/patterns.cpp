5b4208a9736f52285df1ea97db3351c8aec67dda
salmankhan-git <salmankhan.techky@gmail.com>
2026-01-18 20:48:06 +0530

    pattern

--- BASICS/1.2_patterns/patterns.cpp
+++ BASICS/1.2_patterns/patterns.cpp
@@ -17,7 +17,14 @@ void pattern2(int n) {
         cout << endl;
     }
 }
-
+void pattern3(int n) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
 int main()
 {
     int t;
@@ -27,7 +34,8 @@ int main()
         int n;
         cin >> n;
         // pattern1(n);
-        pattern2(n);
+        // pattern2(n);
+        pattern3(n);
     }
     return 0;
 }
