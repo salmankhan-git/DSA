04696ef8019648ebcf6de90f4b267b640fa220a7
salmankhan-git <salmankhan.techky@gmail.com>
2026-02-01 23:18:35 +0530

    basic maths

--- BASICS/maths/math.cpp
+++ BASICS/maths/math.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 int main(){
     
+    // find the digits in a  given number
+    int n;
+    cin>>n;
+    int count=0;
+    while(n>0){
+        count++;
+        n=n/10;
+    }
+    cout<<count<<endl;
+
+    // palindrome number
+    int num;
     
+
     return 0;
 }
