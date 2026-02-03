9a870121e7b05a8d52154d39500492114196989b
salmankhan-git <salmankhan.techky@gmail.com>
2026-02-03 20:59:41 +0530

    basic maths

--- BASICS/maths/math.cpp
+++ BASICS/maths/math.cpp
@@ -21,6 +21,15 @@ int main(){
     int m = log10(n) + 1;
     cout<<m<<endl;
     
+    // reverse number
+    
+    int reverse = 0;
+    while(n>0){
+        int lastdigit = n%10; // extract last digit
+        reverse = reverse*10 + lastdigit; // append last digit
+        n=n/10; // remove last digit
+    }
+    cout<<reverse<<endl;
 
     return 0;
 }
