9b75a1bf956cf44f633307a03fa1ae2a851482e0
salmankhan-git <salmankhan.techky@gmail.com>
2026-02-05 23:41:09 +0530

    basic maths

--- BASICS/maths/math.cpp
+++ BASICS/maths/math.cpp
@@ -63,10 +63,109 @@ int main(){
     }
 
     // all divisors of a number
+
+    // not optimized
     for(int i=1;i<=n;i++){
         if(n%i==0){
             cout<<i<<" ";
         }
     }
+    // optimized
+    vector<int> divisors;
+    // time complexity : O(sqrt(n))
+    for(int i=1;i*i<=n;i++){  //sqrt(n) is function in stl it will take more time to compute so we can use i*i<=n to reduce time
+        if(n%i==0){
+            divisors.push_back(i);
+            if(i!= (n/i)){
+                divisors.push_back(n/i);
+            }
+        }
+    }
+    sort(divisors.begin(),divisors.end());
+    for(auto div : divisors){
+        cout<<div<<" ";
+    }
+    
+  
+    
+    // prime number : a number that has two factors 1 and itself
+    
+    //brute force
+
+    int flag=0;
+    for(int i=2;i<=n;i++){ //we are starting from 2 because 1 is a factor of every number and we are checking for factors other than 1 and itself
+        if(n%i==0){
+            flag+=1;
+        }
+    }
+    if(n<=1){
+        flag=0;
+    }
+    else if(flag==1){
+        cout<<"Prime number"<<endl;
+    }
+    else{
+        cout<<"Not a prime number"<<endl;
+    }
+
+    // optimized approach
+     
+    int isprime=0;
+    for(int i=1;i*i<=n;i++){ // we are starting from 1 because if we start from 2 then we cannot get the factor (itself) as the loop will run only till sqrt(n) and we are checking for factors other than 1 and itself
+        if(n%i==0){
+            isprime+=1;
+            if(i!= (n/i)){
+                isprime+=1;
+            }
+    }
+    if (n<=1){
+        isprime=0;
+    }
+    else if(isprime==2){
+        cout<<"Prime number"<<endl;
+    }
+    else{
+        cout<<"Not a prime number"<<endl;
+    }
+
+
+    // GCD/HCF : greatest common divisor/highest common factor : largest number that divides two numbers
+
+    int a,b;
+    cin>>a>>b;
+    // brute force
+    //time complexity : O(min(a,b))
+    int gcd=1;
+    for(int i=min(a,b);i>=1;i--){ // we are starting from min(a,b) because the gcd cannot be greater than the smaller number
+        if(a%i==0 && b%i==0){
+            gcd=i;
+            break; // this will break the for loop once we find the gcd
+        }
+    }
+    cout<<gcd<<endl;
+
+    // optimized approach : Euclidean algorithm
+    // **** gcd(a,b) = gcd(a-b,b) if a>b or gcd(a,b) = gcd(a,b-a) if b>a
+    // **** gcd(a,0) = a
+    // **** gcd(a,b) = gcd(a%b,b)
+    // time complexity : O(log(min(a,b)))
+    
+    
+    while(b>0 && a>0){
+        if(a>b){
+            a=a%b;
+        }
+        else{
+            b=b%a;
+        }
+    }
+    if(a==0){
+        cout<<b<<endl;
+    }
+    else{
+        cout<<a<<endl;
+    }
+
+
     return 0;
 }
