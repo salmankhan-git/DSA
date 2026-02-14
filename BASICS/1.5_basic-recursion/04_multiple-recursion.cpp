e058efa44458284fc5712dd01ede550f422b4626
salmankhan-git <salmankhan.techky@gmail.com>
2026-02-14 23:59:34 +0530

    multiple-recursion

--- BASICS/1.5_basic-recursion/04_multiple-recursion.cpp
+++ BASICS/1.5_basic-recursion/04_multiple-recursion.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 // multple recursion is a type of recursion where a function calls itself more than once in its body.
 // example of multiple recursion is the fibonacci series where each term is the sum of the two preceding terms.
-
 int fib(int n){
     if(n<=1) return n;
     // return fib(n-1)+fib(n-2);
@@ -11,6 +10,8 @@ int fib(int n){
     int secondLast=fib(n-2);
     return last+secondLast;
 }
+// the time complexity of this approach is approx O(2^n) because each term is calculated multiple times.
+// the space complexity is O(n) because of the recursive call stack.
 
 int main(){
     int n;
---
+++ BASICS/1.6_hashing.cpp/hashing.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+// hashing is a technique used to store and retrieve data in a way that allows for fast access.
+// a hash function is used to convert a key into an index in a hash table where the value is stored.
+// a good hash function should distribute the keys uniformly across the hash table to minimize collisions.
+
+int main(){
+    // counting the frequency of numbers in an array using hashing
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
