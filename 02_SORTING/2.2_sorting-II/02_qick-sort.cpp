71ba56f13fa0a4139ad2104ea5f6d588167cd565
salmankhan-git <salmankhan.techky@gmail.com>
2026-02-26 23:05:00 +0530

    sorting II

--- 02_SORTING/2.2_sorting-II/02_qick-sort.cpp
+++ 02_SORTING/2.2_sorting-II/02_qick-sort.cpp
@@ -1,6 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+void quickSort(vector<int> &arr, int low, int high){
+    if(low < high){
+        int pivot = partition(arr, low, high);
+        quickSort(arr, low, pivot-1);
+        quickSort(arr, pivot+1, high);
+    }
+}
+int partition(vector<int> &arr, int low, int high){
+       int pivot = arr[high];
+    int i = low - 1;
+    for(int j = low; j < high; j++){
+        if(arr[j] < pivot){
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i+1], arr[high]);
+    return i+1;
+}
 int main(){
 
     int n;
