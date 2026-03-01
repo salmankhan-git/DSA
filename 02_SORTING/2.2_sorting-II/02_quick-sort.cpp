2cbdec88a02110d9bb91be794d2eaa2af94b9f39
salmankhan-git <salmankhan.techky@gmail.com>
2026-03-01 23:29:41 +0530

    sorting II

--- 02_SORTING/2.2_sorting-II/02_quick-sort.cpp
+++ 02_SORTING/2.2_sorting-II/02_quick-sort.cpp
@@ -1,24 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int partition(vector<int> &arr, int low, int high){
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while(i<j){
+        while(i<= high -1 &&arr[i] <= pivot ) i++;
+        while(j >= low+1 && arr[j] > pivot) j--;
+
+        if(i < j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
 void quickSort(vector<int> &arr, int low, int high){
     if(low < high){ //if their is a single element then only low = high
-        int pivot = partition(arr, low, high);
-        quickSort(arr, low, pivot-1);
-        quickSort(arr, pivot+1, high);
+        int ption = partition(arr, low, high);
+        quickSort(arr, low, ption-1);
+        quickSort(arr, ption+1, high);
     }
 }
-int partition(vector<int> &arr, int low, int high){
-       int pivot = arr[high];
-    int i = low - 1;
-    for(int j = low; j < high; j++){
-        if(arr[j] < pivot){
-            i++;
-            swap(arr[i], arr[j]);
-        }
-    }
-    swap(arr[i+1], arr[high]);
-    return i+1;
-}
+
+
 int main(){
 
     int n;
@@ -26,6 +32,10 @@ int main(){
     vector<int> arr(n);
     for(int i = 0; i < n; i++)
      cin >> arr[i];
+
+    quickSort(arr, 0, n-1);
+    for(int i = 0; i < n; i++)
+     cout << arr[i] << " ";
 
     return 0;
 }
