0ce20b208926f111613c817c94a5cc6a3d39786f
salmankhan-git <salmankhan.techky@gmail.com>
2026-03-02 23:28:39 +0530

    ARRAY

--- 02_SORTING/2.2_sorting-II/02_quick-sort.cpp
+++ 02_SORTING/2.2_sorting-II/02_quick-sort.cpp
@@ -6,21 +6,22 @@ int partition(vector<int> &arr, int low, int high){
     int i = low;
     int j = high;
 
-    while(i<j){
+    while(i<j){ //we will be running the loop until i is less than j because if i is greater than or equal to j then we have already partitioned the array
+ 
         while(i<= high -1 &&arr[i] <= pivot ) i++;
         while(j >= low+1 && arr[j] > pivot) j--;
 
-        if(i < j) swap(arr[i], arr[j]);
+        if(i < j) swap(arr[i], arr[j]); //if i is less than j then only swap otherwise we will be swapping the elements which are in correct position
     }
-    swap(arr[low], arr[j]);
+    swap(arr[low], arr[j]);//after the loop we will be swapping the pivot element with the element at j because j is the last element which is less than or equal to pivot and all the elements after j are greater than pivot
     return j;
 }
 
 void quickSort(vector<int> &arr, int low, int high){
     if(low < high){ //if their is a single element then only low = high
-        int ption = partition(arr, low, high);
-        quickSort(arr, low, ption-1);
-        quickSort(arr, ption+1, high);
+        int index = partition(arr, low, high);
+        quickSort(arr, low, index-1);
+        quickSort(arr, index+1, high);
     }
 }
 
