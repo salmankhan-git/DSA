d209ec4f99522d8c7ab7f5e9d961d74a665b94c8
salmankhan-git <salmankhan.techky@gmail.com>
2026-03-05 22:28:37 +0530

    ARRAY

--- 03_ARRAY/01_array.cpp
+++ 03_ARRAY/01_array.cpp
@@ -8,10 +8,59 @@ int main() {
         cin >> arr[i];
     }
 
-    // finding largest element in an array
+    // Q1. finding largest element in an array
 
     // brute force approach
-    sort(arr, arr + n);
+    sort(arr, arr + n); //time complexity: O(nlogn)
+    cout << arr[n - 1] << endl;
+     
+    //optimal approach
+    int largest = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    cout << largest << endl; //time complexity: O(n)
+
+
+    //Q2. finding second largest element in an array
+
+    //brute force approach
+    sort(arr, arr + n); //time complexity: O(nlogn+n) = O(nlogn)
+    for (int i = n - 2; i >= 0; i--) {
+        if (arr[i] != arr[n - 1]) {
+            cout << arr[i] << endl;
+            break;
+        }
+    }
+
+    //better approach
+    int largest = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    int second_largest = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > second_largest && arr[i] != largest) {
+            second_largest = arr[i];
+        }
+    }
+    cout << second_largest << endl; //time complexity: O(n+n) = O(n)
+
+    //optimal approach
+    int largest = arr[0], second_largest = INT_MIN;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            second_largest = largest;
+            largest = arr[i];
+        } else if (arr[i] > second_largest && arr[i] < largest) {
+            second_largest = arr[i];
+        }
+    }
+    cout << second_largest << endl; //time complexity: O(n)
 
     return 0;
 }
