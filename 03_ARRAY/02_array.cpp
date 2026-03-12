3da4bd8d994f85ec12a31319485a86d1f4dbdc84
salmankhan-git <salmankhan.techky@gmail.com>
2026-03-12 22:48:07 +0530

    ARRAY

---
+++ 03_ARRAY/02_array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    //Q1. Left rotate an array by d positions
+
+
+    return 0;
+}
