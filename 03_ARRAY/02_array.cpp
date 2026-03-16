c7cb31377be0b48cceb4bd80532839226ff6d23b
salmankhan-git <salmankhan.techky@gmail.com>
2026-03-16 23:29:51 +0530

    ARRAY

--- 03_ARRAY/02_array.cpp
+++ 03_ARRAY/02_array.cpp
@@ -8,11 +8,50 @@ int main(){
         cin>>arr[i];
     }
 
-    //Q1. Left rotate an array by d positions
-    // resultant array after left rotation by d positions
-    // 1. d = 2, arr[] = {1, 2, 3, 4, 5} => {3, 4, 5, 1, 2}
-    // 2. d = 3, arr[] = {1, 2, 3, 4, 5} => {4, 5, 1, 2, 3}
-    // 3. d = 4, arr[] = {1, 2, 3, 4, 5} => {5, 1, 2, 3, 4}
+    //Q1. Left rotate an array by 1 positions
+
+        int temp = arr[0];
+        for (int i=1; i<n ;i++)
+        {
+            arr[i-1] = arr[i];
+        }
+        //or
+        // for (int i=0; i<n-1 ;i++){
+        //     arr[i] = arr[i+1];
+        // }
+        arr[n-1] = temp;
+        // time complexity = O(n)
+        // space complexity = O(1)
+    
+    
+    // Q2. Left rotate an array by D positions
+
+    // brute force approach (using temporary array )
+        int d;
+        cin>>d;
+
+        //reducing d if it is greater than n
+        d = d%n;
+       
+        // store the first d elements in a temporary array 
+        int temp[d];
+        for (int i=0; i<d; i++){
+            temp[i] = arr[i];
+        }
+
+        // shift the remaining n-d elements to the left by d positions
+        for (int i=d; i<n; i++){
+            arr[i-d] = arr[i];
+        }
+
+        // copy the d elements from the temporary array to the end of the original array
+        for (int i=0; i<d; i++){
+            arr[n-d+i] = temp[i];
+        }
+        // time complexity = O(d) + O(n-d) + O(d) = O(n+d) = O(n)
+        // space complexity = O(d)
+
 
     return 0;
+
 }
