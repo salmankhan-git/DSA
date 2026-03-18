33f0f8d4396d447f78b73208098f2f541ff88b04
salmankhan-git <salmankhan.techky@gmail.com>
2026-03-18 16:35:20 +0530

    ARRAY

--- 03_ARRAY/02_array.cpp
+++ 03_ARRAY/02_array.cpp
@@ -25,7 +25,7 @@ int main(){
         // space complexity = O(1)
     
     
-    // Q2. Left rotate an array by D positions
+    // Q2(a). Left rotate an array by D positions
 
     // brute force approach (using temporary array )
         int d;
@@ -73,6 +73,50 @@ int main(){
         // if(n == 0) return 0;  it is important to handle the case when n is 0 to avoid division by zero error in the line where we calculate d % n. If n is 0, we can simply return 0 since there are no elements to rotate.
         // d = ((d % n) + n) % n; this line is used to handle the case when d is negative. It ensures that d is always a non-negative value less than n, which is necessary for the rotation to work correctly. If d is negative, it will be converted to a positive equivalent by adding n to it before taking the modulus with n.
         // time complexity = O(d)+O(n-d)+O(n) = O(n)
+        // space complexity = O(1)
+
+        // Q2(b). right rotate an array by D positions
+
+    // brute force approach (using temporary array )
+       
+       
+        // store the first d elements in a temporary array 
+        vector<int> temp(d);
+        for (int i=n-d; i<n; i++){
+            temp[&i-(n-d)] = arr[i];
+        }
+
+        // shift the remaining n-d elements to the right by d positions
+        for (int i=n-d-1; i>=0; i--){
+            arr[i+d] = arr[i];
+        }
+
+        // copy the d elements from the temporary array to the start of the original array
+        for (int i=0; i<d; i++){
+            arr[i] = temp[&i];
+        }
+        // time complexity = O(d) + O(n-d) + O(d) = O(n+d) = O(n)
+        // space complexity = O(d)
+
+        // optimal approach (using reverse method)
+        auto reverserr = [&](vector<int> &arr, int start, int end){
+            while(start<=end){
+                int temp = arr[start];
+                arr[start++] = arr[end];
+                arr[end--] = temp;
+                // start++;
+                // end--;
+            }
+        }
+        ;
+        // reverse the entire array
+        reverserr(arr, 0, n-1);
+        // reverse the first d elements of the array
+        reverserr(arr, 0, d-1);
+        // reverse the remaining elements of the array
+        reverserr(arr, d, n-1);
+
+        // time complexity = O(n)+O(d)+O(n-d) = O(n)
         // space complexity = O(1)
         
     return 0;
