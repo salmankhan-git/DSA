a20f3a0077592c2a45449cddd7ab7eae0231c38b
salmankhan-git <salmankhan.techky@gmail.com>
2026-03-31 23:21:41 +0530

    ARRAY

--- 03_ARRAY/04_array.cpp
+++ 03_ARRAY/04_array.cpp
@@ -26,8 +26,28 @@ int main(){
         }
     }
     cout<<"Largest subarray with sum K is: "<<max_len<<endl;
+    //time complexity : O(n^2)
+    //space complexity : O(1)
 
+    //Better approach (using hashing)
+    // we will use prefix sum and hash map to store the prefix sum and its index
+    // prefix sum : sum of elements from index 0 to i 
 
-
+    // prefix sum = x
+    // x = (x-k) + k
+    // then we can find the subarray with sum k by checking if (x - k) exists in the hash map
+    // if it exists then we can find the length of the subarray by taking the difference of the current index and the index of (x-k) in the hash map
+    
+    map<int, int> mp; // to store prefix sum and its index
+    int prefix_sum = 0;
+    int max_len2 = 0;
+    for(int i=0; i<n; i++){
+        prefix_sum += arr[i];
+        if(prefix_sum ==k){
+            max_len2 = max(max_len2, i+1);
+        }
+        
+    }
+    
     return 0;
 }
