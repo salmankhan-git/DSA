f3a10fb973d2e348601fe895dc18dd06e20bd9a3
salmankhan-git <salmankhan.techky@gmail.com>
2026-04-15 23:45:11 +0530

    ARRAY

--- 03_ARRAY/11_array.cpp
+++ 03_ARRAY/11_array.cpp
@@ -23,5 +23,51 @@ int main(){
     }
     //time complexity: O(n) (for next_permutation)
     //space complexity: O(n) (for vector)
+
+
+    // Optimal approach
+    // 1. Traverse from the end and find the first pair of adjacent elements such that arr[i] < arr[i + 1]. Let us call this index i. If no such index exists, then this is the last permutation and just return the reverse of the array.
+    // 2. Traverse from the end again and find the first index j such that arr[i] < arr[j]. Since we are traversing from the end, we are guaranteed to find such an index.
+    // 3. Swap arr[i] and arr[j].
+    // 4. Reverse the subarray arr[i + 1...n - 1] .
+
+    //step 1 find the first pair of adjacent elements such that arr[i] < arr[i + 1]
+    int i = -1;
+    for (int k = n - 2; k >= 0; k--){
+        if (arr[k] < arr[k + 1]){
+            i = k;
+            break;
+        }
+    }
+    // If no such index is found, this is the last permutation
+    if (i == -1) {
+        reverse(arr, arr + n);
+    }
+    
+    
+    else {
+        // Step 2: Find the largest index j such that arr[i] < arr[j]
+        int j ;
+        for (int k = n - 1; k > i; k--) {
+            if (arr[i] < arr[k]) {
+                j = k;
+                break;
+            }
+        }
+
+        // Step 3: Swap arr[i] and arr[j]
+        swap(arr[i], arr[j]);
+
+        // Step 4: Reverse the subarray from i + 1 to n - 1
+        reverse(arr + i + 1, arr + n);
+    }
+
+    // Print the next permutation
+    for (int k = 0; k < n; k++) {
+        cout << arr[k] << " ";
+    }
+
+    //time complexity: O(n for finding i) + O(n for finding j) + O(1 for swapping) + O(n for reversing) = O(3n)
+    //space complexity: O(1)
     return 0;
 }
